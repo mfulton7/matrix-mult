bddcf96560302443a9afeb23de889354c725ef26
mfulton7 <mfulton7@kent.edu>
2025-03-19 22:00:58 -0400

    implemented print functions, added content to main fo rtesting

--- matrix-mult/matrix.cpp
+++ matrix-mult/matrix.cpp
@@ -1,9 +1,15 @@
 #include "matrix.h"
 
-matrix::matrix(const std::vector<std::vector<int>>& data)
+matrix::matrix(const std::vector<std::vector<int>>& passed_data)
 {
+	this->data = passed_data;
 }
 
 void matrix::print()
 {
+	for (int i = 0; i < this->data.size(); i++) {
+		for (int j = 0; j < data[i].size(); j++) {
+			std::cout << data[i][j] << std::endl;
+		}
+	}
 }
--- matrix-mult/matrix.h
+++ matrix-mult/matrix.h
@@ -7,8 +7,9 @@
 class matrix
 {
 public:
+	std::vector<std::vector<int>> data;
 	// constructor
-	matrix(const std::vector<std::vector<int>>& data);
+	matrix(const std::vector<std::vector<int>>& passed_data);
 
 	// allows for visualizing data in matrix
 	void print();
